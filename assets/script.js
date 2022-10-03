2f698080ff7b030a9ea8c5ba35a5c5f29bc03aa8
Jon <Jon@Minettes-MacBook-Pro.local>
2022-10-03 18:24:43 -0400

    timeblocks created  in index fuctinallity added to buttons

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,32 @@
 //Get current date and formate it propperly to jumbotron class
-const currentDay = document.getElementById("currentDay")
-var today = moment().format("dddd, MMMM Do YYYY")
-currentDay.innerHTML = today
 
-//crating the time blocks with the hour and save button
+var today = moment().format("dddd, MMMM Do YYYY")
+$('#currentDay').text(today)
+
+function saveBtnFunc(){ 
+    
+    const text = $(this).siblings(".description").val()
+    console.log(text)
+    const time = $(this).siblings(".description").attr("id")
+    console.log(time)
+    localStorage.setItem(time, text)
+}
+$(".saveBtn").on("click",saveBtnFunc)
+
+
+function rowRender(){
+    for (let i = 9; i <= 17; i++) {
+        $(`#hour-${i}`).val(localStorage.getItem(`hour-${i}`))
+
+        
+    }
+}
+rowRender()
+
+function rowColor(){
+    let currentTime = moment().hour()
+    $('.description').each(function(){
+        console.log($(this).attr('id'))
+    })
+}
+rowColor()
